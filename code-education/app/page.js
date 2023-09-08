2aebd2834a4673d2e12a581e914b783929ff92d5
Brosey27 <140609566+Brosey27@users.noreply.github.com>
2023-09-08 16:19:04 +1000

    Add 6 more sections, imports relevant images, styledelements to compliment the theme of each image. Add responsive design for all display sizes

--- code-education/app/page.js
+++ code-education/app/page.js
@@ -9,6 +9,13 @@ import curriculum from "../public/curriculum.svg";
 import HTML from "../public/HTML.svg";
 import CSS from "../public/css.svg";
 import JS from "../public/Javascript.png";
+import GIT from "../public/git.png";
+import PM from  "../public/packagemanager.svg";
+import FRAMEWORKS from "../public/frameworks.png";
+import CSSFRAME from "../public/cssframe.png";
+import CSSPRE from "../public/csspre.svg";
+import TYPE from "../public/type.svg";
+import SSR from "../public/SSR.png";
 
 export default function Home() {
 const [nav, setNav] = useState(true);
@@ -230,6 +237,218 @@ Visit the following resources to learn more:</p>
   <a href="https://www.javascripttutorial.net/javascript-fetch-api/" className="mx-2 my-2">
     <button className="bg-yellow-500 w-[200px] rounded-md font-medium py-3 text-white">
       Fetch API
+    </button>
+  </a>
+</div>
+
+          </div>
+          </div>
+          {/* Learn about git/github */}
+          <div className="text-white mt-[-150px]">
+          <div className="maxw-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+            
+          
+         
+              <Image alt="CSS2" src={GIT} className="w-[500px] mx-auto mt-96" />
+          
+          <h1 className=' md:text-3xl sm:text-2xl text-2xl font-bold md:py-6'>Git/GitHub</h1>
+          <p className='text-md'>Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.
+
+Visit the following resources to learn more:</p>
+          <div className="flex flex-wrap justify-center">
+  <a href="https://www.youtube.com/watch?v=SWYqp7iY_Tc" className="mx-2 my-2">
+    <button className="bg-purple-500 w-[200px] rounded-md font-medium py-3 text-white">
+      Git/GitHub basics
+    </button>
+  </a>
+  <a href="https://www.atlassian.com/git" className="mx-2 my-2">
+    <button className="bg-purple-500 w-[200px] rounded-md font-medium py-3 text-white">
+      Git Tutorials
+    </button>
+  </a>
+  <a href="https://cs.fyi/guide/git-cheatsheet" className="mx-2 my-2">
+    <button className="bg-purple-500 w-[200px] rounded-md font-medium py-3 text-white">
+      Git Cheatsheet
+    </button>
+  </a>
+</div>
+
+          </div>
+          </div>
+          {/* Learn about Package Managers */}
+          <div className="text-white mt-[-150px]">
+          <div className="maxw-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+            
+          
+         
+              <Image alt="CSS2" src={PM} className="w-[500px] mx-auto mt-96" />
+          
+          <h1 className=' md:text-3xl sm:text-2xl text-2xl font-bold md:py-6'>Package Managers</h1>
+          <p className='text-md'>Package managers allow you to manage the dependencies that allow your project to work correctly.
+
+Visit the following resources to learn more:</p>
+          <div className="flex flex-wrap justify-center">
+  <a href="https://nodesource.com/blog/an-absolute-beginners-guide-to-using-npm/" className="mx-2 my-2">
+    <button className="bg-green-500 w-[200px] rounded-md font-medium py-3 text-white">
+      NPM
+    </button>
+  </a>
+  <a href="https://blog.bitsrc.io/pnpm-javascript-package-manager-4b5abd59dc9" className="mx-2 my-2">
+    <button className="bg-green-500 w-[200px] rounded-md font-medium py-3 text-white">
+      PNPM
+    </button>
+  </a>
+  <a href="https://classic.yarnpkg.com/en/docs/getting-started" className="mx-2 my-2">
+    <button className="bg-green-500 w-[200px] rounded-md font-medium py-3 text-white">
+      YARN
+    </button>
+  </a>
+</div>
+
+          </div>
+          </div>
+          {/* Learn about Frameworks */}
+          <div className="text-white mt-[-150px]">
+          <div className="maxw-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+            
+          
+         
+              <Image alt="CSS2" src={FRAMEWORKS} className="w-[500px] mx-auto mt-96" />
+          
+          <h1 className=' md:text-3xl sm:text-2xl text-2xl font-bold md:py-6'>Frameworks</h1>
+          <p className='text-md'>Web frameworks are designed to write web applications. Frameworks are collections of libraries that aid in the development of a software product or website. Frameworks for web application development are collections of various tools. Frameworks vary in their capabilities and functions, depending on the tasks set. They define the structure, establish the rules, and provide the development tools required.
+
+Visit the following resources to learn more:</p>
+          <div className="flex flex-wrap justify-center">
+  <a href="https://egghead.io/courses/the-beginner-s-guide-to-react" className="mx-2 my-2">
+    <button className="bg-lime-500 w-[200px] rounded-md font-medium py-3 text-white">
+      React
+    </button>
+  </a>
+  <a href="https://www.youtube.com/watch?v=FXpIoQ_rT_c" className="mx-2 my-2">
+    <button className="bg-lime-500 w-[200px] rounded-md font-medium py-3 text-white">
+      Vue.js
+    </button>
+  </a>
+  <a href="https://angular.io/start" className="mx-2 my-2">
+    <button className="bg-lime-500 w-[200px] rounded-md font-medium py-3 text-white">
+      Angular
+    </button>
+  </a>
+</div>
+
+          </div>
+          </div>
+          {/* Learn about CSS Frameworks */}
+          <div className="text-white mt-[-150px]">
+          <div className="maxw-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+            
+          
+         
+              <Image alt="CSS2" src={CSSFRAME} className="w-[500px] mx-auto mt-96" />
+          
+          <h1 className=' md:text-3xl sm:text-2xl text-2xl font-bold md:py-6'>CSS Frameworks</h1>
+          <p className='text-md'>The way we write CSS in our modern front-end applications is completely different from how we used to write CSS before. There are methods such as Styled Components, CSS Modules, Styled JSX, Emotion, etc.
+
+Visit the following resources to learn more:</p>
+          <div className="flex flex-wrap justify-center">
+  <a href="https://www.youtube.com/watch?v=lCxcTsOHrjo" className="mx-2 my-2">
+    <button className="bg-pink-500 w-[200px] rounded-md font-medium py-3 text-white">
+      Tailwind CSS
+    </button>
+  </a>
+  <a href="https://www.radix-ui.com/primitives/docs/overview/introduction" className="mx-2 my-2">
+    <button className="bg-pink-500 w-[200px] rounded-md font-medium py-3 text-white">
+      Radix UI
+    </button>
+  </a>
+  <a href="https://ui.shadcn.com/" className="mx-2 my-2">
+    <button className="bg-pink-500 w-[200px] rounded-md font-medium py-3 text-white">
+      Shadcn UI
+    </button>
+  </a>
+</div>
+
+          </div>
+          </div>
+          {/* Learn CSS Preprocessors */}
+          <div className="text-white mt-[-150px]">
+          <div className="maxw-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+            
+          
+         
+              <Image alt="CSS2" src={CSSPRE} className="w-[500px] mx-auto mt-96" />
+          
+          <h1 className=' md:text-3xl sm:text-2xl text-2xl font-bold md:py-6'>CSS Preprocessors</h1>
+          <p className='text-md'>CSS Preprocessors are scripting languages that extend the default capabilities of CSS. They enable us to use logic in our CSS code, such as variables, nesting, inheritance, mixins, functions, and mathematical operations.
+
+Visit the following resources to learn more:</p>
+          <div className="flex flex-wrap justify-center">
+  <a href="https://www.freecodecamp.org/news/css-preprocessors/" className="mx-2 my-2">
+    <button className="bg-violet-500 w-[200px] rounded-md font-medium py-3 text-white">
+      Preprocessor Basics
+    </button>
+  </a>
+  <a href="https://www.youtube.com/watch?v=_a5j7KoflTs" className="mx-2 my-2">
+    <button className="bg-violet-500 w-[200px] rounded-md font-medium py-3 text-white">
+      Sass Basics
+    </button>
+  </a>
+  <a href="https://postcss.org/" className="mx-2 my-2">
+    <button className="bg-violet-500 w-[200px] rounded-md font-medium py-3 text-white">
+      PostCSS
+    </button>
+  </a>
+</div>
+
+          </div>
+          </div>
+          {/* Learn Type Checkers */}
+          <div className="text-white mt-[-150px]">
+          <div className="maxw-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+            
+          
+         
+              <Image alt="CSS2" src={TYPE} className="w-[500px] mx-auto mt-96" />
+          
+          <h1 className=' md:text-3xl sm:text-2xl text-2xl font-bold md:py-6'>Type Checkers</h1>
+          <p className='text-md'>Type checker helps developers write code with fewer bugs by adding types to their code, trying to catch type errors within your code, and then removing them during compile time. Flow and TypeScript are two popular static type checkers for JavaScript however I recommend Typescript.</p>
+          <div className="flex flex-wrap justify-center">
+  <a href="https://www.youtube.com/watch?v=BwuLxPH8IDs" className="mx-2 my-2">
+    <button className="bg-blue-500 w-[200px] rounded-md font-medium py-3 text-white">
+      Typescript
+    </button>
+ </a>
+</div>
+
+          </div>
+          </div>
+          {/* Learn SSR */}
+          <div className="text-white mt-[-150px]">
+          <div className="maxw-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+            
+          
+         
+              <Image alt="CSS2" src={SSR} className="w-[500px] mx-auto mt-96" />
+          
+          <h1 className=' md:text-3xl sm:text-2xl text-2xl font-bold md:py-6'>Server Side Rendering or SSR</h1>
+          <p className='text-md'>Server-side rendering refers to the process that the service side completes the HTML structure splicing of the page, sends it to the browser, and then binds the status and events for it to become a fully interactive page.
+
+Visit the following resources to learn more:</p>
+          <div className="flex flex-wrap justify-center">
+  <a href="https://www.educative.io/answers/what-is-server-side-rendering" className="mx-2 my-2">
+    <button className="bg-violet-500 w-[200px] rounded-md font-medium py-3 text-white">
+      What is SSR?
+    </button>
+  </a>
+  <a href="https://masteringnextjs.com/" className="mx-2 my-2">
+    <button className="bg-violet-500 w-[200px] rounded-md font-medium py-3 text-white">
+      Next.js
+    </button>
+  </a>
+  <a href="https://remix.run/" className="mx-2 my-2">
+    <button className="bg-violet-500 w-[200px] rounded-md font-medium py-3 text-white">
+      Remix
     </button>
   </a>
 </div>
